d795991bdc2af4bff83b57c3fca73b205533c346
thiscesar <cesarldsousa@gmail.com>
2025-06-21 11:55:09 -0300

    add first loop for testing

--- src/infiniteSlider.js
+++ src/infiniteSlider.js
@@ -1,5 +1,32 @@
 (function ($) {
   $.fn.infiniteSlider = function (options) {
-    console.log('im a slider, but infinite!');
+    const settings = $.extend(
+      {
+        speed: 20, // loop duration
+      },
+      options
+    );
+
+    return this.each(function () {
+      const $container = $(this);
+      const $images = $container.children().clone();
+
+      if ($container.data("infinite-slider-initialized")) {
+        return;
+      }
+
+      $container.data("infinite-slider-initialized", true);
+
+      const $wrapper = $('<div class="slider-wrapper"></div>');
+      const $track = $('<div class="slider-track"></div>').css({
+        animation: `scroll ${settings.speed}s linear infinite`,
+      });
+
+      $images.addClass("slider-logo");
+      $track.append($images.clone(), $images.clone());
+
+      $wrapper.append($track);
+      $container.empty().append($wrapper);
+    });
   };
 })(jQuery);
